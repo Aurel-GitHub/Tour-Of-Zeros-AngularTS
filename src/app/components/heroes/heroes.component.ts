cfe3db19722b394032f511cbe4f4dd1e8c16c593
Aurelien__ <aurelien@expateo.com>
2021-09-29 10:36:18 +0200

    updated modal

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { heroesData } from 'src/data/Heroes.data';
 import { MatDialog } from '@angular/material/dialog';
 import { HeroesDetailsModalComponent } from 'src/app/modal/heroes-details-modal/heroes-details-modal.component';
-import { HeroesService } from 'src/services/Heroes.services';
-import { Subscription } from 'rxjs';
-import { Hero } from 'src/interfaces/IHero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -17,7 +14,6 @@ export class HeroesComponent implements OnInit {
 
   hero: [];
   heroes = heroesData;
-  subscription: Subscription;
 
   ngOnInit(): void {
   }
@@ -28,8 +24,9 @@ export class HeroesComponent implements OnInit {
 
   openDialog(name: string, description: string, image:string): void {
     this.dialog.open(HeroesDetailsModalComponent, {
-      width: '330px',
-      height: '400px',
+      width: '600px',
+      height: '500px',
+      maxHeight: '600px',
       data: {
         name: name,
         description: description,
